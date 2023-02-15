168b428b815f09056b59dd240c5f45391aff2a9b
nachogarcia <iggguvs@uma.es>
2023-02-15 19:00:49 +0100

    test EdadMediaDeCadaGenero correcto

--- src/test/java/PersonTests/PersonTest.java
+++ src/test/java/PersonTests/PersonTest.java
@@ -2,6 +2,11 @@ package PersonTests;
 import org.example.Person;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PersonTest {
@@ -15,6 +20,24 @@ public class PersonTest {
         assertEquals(name, persona.name());
         assertEquals(age, persona.age());
         assertEquals(gender, persona.gender());
+    }
+
+    @Test
+    public void testEdadMediaDeCadaGenero(){
+        /**
+         * Lista de personas, 2 hombres y 2 mujeres, cuyas edades medias deberian ser 30 y 40 respectivamente
+         */
+        List<Person> personas = new ArrayList<>(Arrays.asList(
+                new Person("Nacho",20,"Male"),
+                new Person("Miguel,",40,"Male"),
+                new Person("Celia",30,"Female"),
+                new Person("Alicia",50,"Female")
+        ));
+        double [] expected = new double[]{30,40};
+
+        Person person = new Person("",0,"");
+        assertArrayEquals(expected,person.averageAgePerGender(personas));
+
     }
 
 }
