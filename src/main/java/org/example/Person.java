9eb064b60a72c1626b13f191e5e9573aeabf46b2
nachogarcia <iggguvs@uma.es>
2023-02-15 18:03:38 +0100

    Enunciado copiado

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,2 +1,45 @@
-package org.example;public class Person {
+package org.example;
+
+import java.util.List;
+
+/**
+ * Class representing a person with a name, age and gender
+ *
+ * @author Antonio J. Nebro
+ */
+public class Person {
+    private final String name;
+    private final int age;
+    private final String gender;// Male, Female
+
+    /**
+     * Constructs a person with a name, age and gender.
+     *
+     * @param name the name of the person
+     * @param age the age of the person
+     * @param gender the gender of the person
+     */
+    public Person(String name, int age, String gender){
+
+    }
+    public String name(){
+        return name;
+    }
+    public int age(){
+        return age;
+    }
+    public String gender(){
+        return gender;
+    }
+    /**
+     * Computes the average age of male and female persons in a list and returns the result in
+     * an array of two elements (the first element is the male mean age and the second one is the
+     * female mean age)
+     *
+     * @param persons
+     * @return
+     */
+    public double[] avergaeAgePerGender(List<Person> persons){
+        return null;
+    }
 }
